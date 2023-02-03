8da920c47e307d0427f98de124887b8042d04d6d
“Aashis” <thapaaashis20@gmail.com>
2023-02-03 11:18:55 +0545

    Align layout to center

--- app/src/main/java/com/jetpack/loginsignupjetpackcompose/ui/login/LoginScreen.kt
+++ app/src/main/java/com/jetpack/loginsignupjetpackcompose/ui/login/LoginScreen.kt
@@ -1,22 +1,24 @@
 package com.jetpack.loginsignupjetpackcompose.ui.login
 
-import android.widget.EditText
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.jetpack.loginsignupjetpackcompose.R
+import com.jetpack.loginsignupjetpackcompose.ui.theme.PrimaryColor
 import com.jetpack.loginsignupjetpackcompose.ui.theme.White
+import java.util.*
 
 
 /**
@@ -32,24 +34,56 @@ fun LoginScreen() {
     ) {
         val username = remember { mutableStateOf(TextFieldValue()) }
         val password = remember { mutableStateOf(TextFieldValue()) }
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Box() {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_jetpack_compose),
-                    contentDescription = "This is logo"
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(modifier = Modifier.height(60.dp))
+            Box {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_login),
+                    contentDescription = "This is logo",
+                    tint = PrimaryColor,
+                    modifier = Modifier.size(100.dp)
                 )
             }
+            Spacer(modifier = Modifier.height(60.dp))
 
-            TextField(value = username.value,
+            Text(
+                text = "Login to your Account",
+                textAlign = TextAlign.Start
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+
+            OutlinedTextField(value = username.value,
                 label = { Text(text = "Email") },
                 onValueChange = { username.value = it })
 
-            TextField(value = password.value, onValueChange = { password.value = it }, label = {
+            Spacer(modifier = Modifier.height(20.dp))
+
+            OutlinedTextField(value = password.value,
+                onValueChange = { password.value = it },
+                label = {
+                    Text(
+                        text = "Password"
+                    )
+                })
+
+            Spacer(modifier = Modifier.height(40.dp))
+            Button(
+                onClick = { }, colors = ButtonDefaults.buttonColors(backgroundColor = PrimaryColor)
+            ) {
                 Text(
-                    text = "Password"
+                    modifier = Modifier.padding(
+                        horizontal = 52.dp, vertical = 8.dp
+                    ),
+                    text = "Sign in", color = White,
                 )
-            })
+            }
 
         }
     }
 }
+
